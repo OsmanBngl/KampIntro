b6375f06eb7b90ce7d57d3ae2339751fa9a88816
Osman Bingul <58314047+Osman686@users.noreply.github.com>
2021-02-13 21:31:33 +0300

    Ders Sonu

---
+++ koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string> { "Osman", "Emre", "Enes", "Fatih", "Burak" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[4]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[5]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
