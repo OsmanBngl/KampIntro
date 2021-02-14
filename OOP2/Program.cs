c59969f50bda5eab844b4334cc7145a4c47545c6
Osman Bingul <58314047+Osman686@users.noreply.github.com>
2021-02-14 19:52:02 +0300

    14.02.21 Ders Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "osman";
+            musteri1.Soyadi = "bingül";
+            musteri1.TcNo = "32131231";
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 3;
+            musteri2.MusteriNo = "32131231";
+            musteri2.SirketAdi = "Bingul Yazılım";
+            musteri2.VergiNo = "321321312312";
+
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+        }
+    }
+}
