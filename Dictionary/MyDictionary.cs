fbef94bc773194ad2119e273ff7257e28352146b
Osman Bingul <58314047+Osman686@users.noreply.github.com>
2021-02-14 16:28:37 +0300

    4.hafta Ödev

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<O>
+    {
+        O[] items;
+
+     public MyDictionary()
+        {
+            items = new O[0];
+        }
+        public void Add(O item)
+        {
+            O[] tempArray = items;
+            items = new O[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+        public O[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary< string > mydictionary = new MyDictionary<string>();
+            mydictionary.Add("OSMAN");
+            Console.WriteLine(mydictionary.Length);
+            mydictionary.Add("Kerpeten");
+            Console.WriteLine(mydictionary.Length);
+            mydictionary.Add("Ali");
+            Console.WriteLine(mydictionary.Length);
+
+            foreach (var isim in mydictionary.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
